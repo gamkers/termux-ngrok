463a2a200a014026f31a175c96b0b9e51a307145
AKASH <87299311+gamkers@users.noreply.github.com>
2024-02-20 17:28:29 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,14 +2,12 @@
 rm $PREFIX/bin/ngrok
 rm -rf $PREFIX/share/ngrok
 mkdir -p $PREFIX/share/ngrok
-cp get-ngrok.sh $PREFIX/share/ngrok
-cp ngrok $PREFIX/bin
 apt update && apt upgrade -y
 apt install -y proot wget resolv-conf
 apt clean
 apt autoremove
 cd $PREFIX/share/ngrok
-bash get-ngrok.sh
+tar xvzf ngrok-v3-stable-linux-arm64.tgz -C $PREFIX/bin/
 echo -e "\e[1;32mNgrok installed sucessfull!"
 echo "Run : ngrok "
 echo " To use ngrok"
